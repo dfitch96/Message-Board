7b0ed45ec3e1facf7701273fd84322d8fc689caa
Dylan Fitch <kkarl30@gmail.com>
2025-05-03 11:22:49 -0400

    removed post install script

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,8 +1,6 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_DATABASE} = process.env;
-
 const messages = [
   {
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
